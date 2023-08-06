f8ad33b008f149627cc5bc164ff7068045e55508
anhtuannguyen <anhnguyensgu@gmail.com>
2023-08-06 23:58:21 +0700

    new config

--- nvim/lua/colorschemes.lua
+++ nvim/lua/colorschemes.lua
@@ -1,14 +1,22 @@
 require('rose-pine').setup({
-  disable_background = true
-
+    disable_background = true,
+    highlight_groups = {
+        CursorLine = { bg = 'foam', blend = 10, fg = 'none' },
+        TelescopeBorder = { fg = "highlight_high", bg = "none" },
+        TelescopeNormal = { bg = "none" },
+        TelescopePromptNormal = { bg = "base" },
+        TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+        TelescopeSelection = { fg = "text", bg = "base" },
+        TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+    },
 })
 
 function SetUpMyBG(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 SetUpMyBG()
